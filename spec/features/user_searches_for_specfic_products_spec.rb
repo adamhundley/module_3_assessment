78c0b7b5cf9b7ebcb2dfb641dbebdf69057c20b8
Adam Hundley <adamhundley@gmail.com>
2016-04-26 11:31:33 -0600

    working on second user story

--- spec/features/user_searches_for_specfic_products_spec.rb
+++ spec/features/user_searches_for_specfic_products_spec.rb
@@ -8,11 +8,11 @@ RSpec.feature "UserSearchesForProducts", type: :feature do
   scenario "User visits home page and searches for a product" do
     VCR.use_cassette("feature#product_search") do
       visit "/"
-      fill_in "product", with: "sennheiser"
+      fill_in "product", with: "sennheiser headphones white"
       click_on "search"
 
       expect(current_path).to eq "/search"
-      within("div#product-9678429") do
+      within("div#product-9068155") do
         expect(page).to have_content "Sennheiser - Camera-Mount Wireless Microphone System - Black"
         expect(page).to have_content "$629.95"
         expect(page).to have_content "SKU: 9678429 "
